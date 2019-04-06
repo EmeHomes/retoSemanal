0be700d9d7548e1b7f049bc938d8e9b0c34ed8c4
emehomes <mscasas.94@gmail.com>
2019-04-06 15:39:43 +0200

    Agregado nuevo campo a la clase coche

--- Sanchez_casas_maria_Prog7_Caso1/src/Coche.java
+++ Sanchez_casas_maria_Prog7_Caso1/src/Coche.java
@@ -4,16 +4,27 @@ public class Coche {
 	String matricula;
 	String modelo;
 	String color;
+	String tipoVehiculo;
 
-	public Coche(String matricula, String modelo, String color) {
+	
+	public Coche(String matricula, String modelo, String color, String tipoVehiculo) {
 		this.matricula = matricula;
 		this.modelo = modelo;
 		this.color =color;
+		this.tipoVehiculo =tipoVehiculo;
 	}
 
+	public String getInfo() {
+		String mensaje = "Esta es la matrícula contenida: " + this.matricula + 
+						 " Este es el modelo contenido: " + this.modelo +
+		 				 " Este es el color contenido: " + this.color;
+		return mensaje;
+	}
+	
 	public String getMatricula() {
 		return matricula;
 	}
+	
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
 	}
@@ -25,17 +36,21 @@ public class Coche {
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+	
 	public String getColor() {
 		return color;
 	}
+	
 	public void setColor (String color) {
 		this.color = color;
 	}
 	
-	public String getInfo() {
-		String mensaje = "Esta es la matrícula contenida: " + this.matricula + 
-						 " Este es el modelo contenido: " + this.modelo +
-		 				 " Este es el color contenido: " + this.color;
-		return mensaje;
+	public String getTipoVehiculo() {
+		return tipoVehiculo;
 	}
+
+	public void setTipoVehiculo(String tipoVehiculo) {
+		this.tipoVehiculo = tipoVehiculo;
+	}
+	
 }
